5692baf82c1f665404173e19591d095e8ebf9c38
bai648080679 <17600825103@163.com>
2017-09-07 20:21:05 +0800

    Inital commit

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#pop_meum").click(function(){
+		$("#nav-block").toggleClass("none");
+	});
+	$("#close").click(function(){
+		$("#nav-block").toggleClass("none");
+	});
+});
